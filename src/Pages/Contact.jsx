6ca37db3e6fbf8b56ad030d693e439d1b8dfcf1e
anchalraj31082004 <anchalraj20045@gmail.com>
2023-07-25 19:53:13 +0530

    Completed 404 Error page and Contactus page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="flex flex-col gap-10 ">
+      <h2 className="text-4xl font-medium text-center mt-5 text-white underline underline-offset-8 hover:text-yellow-400">Feel free to share your reviews</h2>
+      <div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117223.65983299309!2d85.2389251528664!3d23.343340860600705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e104aa5db7dd%3A0xdc09d49d6899f43e!2sRanchi%2C%20Jharkhand!5e0!3m2!1sen!2sin!4v1690280243901!5m2!1sen!2sin"
+          width="100%"
+          height="320"
+          style={{border:0}}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <div className="flex items-center justify-center mb-5">
+        <div className="h-[400px] w-[550px] bg-cyan-900 flex flex-col gap-4 items-center justify-center px-10 rounded-xl text-slate-600">
+          <h3 className="text-3xl font-semibold text-white">Contact us</h3>
+          <input
+            className="py-3 px-24 rounded-md text-center outline-none"
+            type="text"
+            placeholder="Name"
+            name="username"
+            required
+            autoComplete="off"
+          />
+          <input
+            className="py-3 px-24 rounded-md text-center outline-none"
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+          <textarea
+            className="py-3 px-24 rounded-md text-center outline-none"
+            name="message"
+            cols="20"
+            placeholder="Message"
+            rows="3"
+            autoComplete="off"
+            required
+          />
+          <div>
+            <Button text={"Send"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
-    <div>Error</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center">
+      <div className="h-[380px] w-[500px] bg-cyan-200 flex flex-col gap-4 items-center justify-center px-10 rounded-xl text-slate-600">
+        <h1 className="text-5xl font-bold">404</h1>
+        <h4 className="text-3xl font-medium">OH! You are lost.</h4>
+        <p className="text-base font-light">
+          The page you are lokking for does not exist. How you got here is a
+          mystry. But you can click the button below to go back to Home page.
+        </p>
+        <div onClick={() => navigate("/")}>
+          <Button text={"Home"} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
